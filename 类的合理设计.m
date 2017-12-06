0fb530fb31c1f0649b079200724dfdcf13dc5956
captainTao <578628438@qq.com>
2017-12-06 15:25:21 +0800

    ..

--- 类的合理设计.m
+++ 类的合理设计.m
@@ -1,6 +1,6 @@
 //
 //  类的合理设计.m
-//  
+//  需求：人遛狗
 //
 //  Created by captain on 2017/12/5.
 //
@@ -20,39 +20,82 @@ typedef enum {
 } Color;
 
 #import <Foundation/Foundation.h>
-@interface Student :NSObject
+
+@interface Dog : NSObject  //狗的属性
+{
+@public
+    Color curclor;//毛色
+    double weight;//体重
+}
+- (void)run;
+- (void)eat;
+@end
+
+@implementation Dog
+- (void)run{
+    weight -= 1;
+    NSLog(@"狗跑完这次后的体重%f", weight);
+}
+- (void)eat{
+    weight += 1;
+    NSLog(@"狗吃完这次后的体重%f", weight);
+}
+@end
+
+@interface Student :NSObject  //人的属性:吃，跑，遛狗，喂狗
 {
     @public
     Sex sex; //性别
     Date birthday;//生日
     double weight;//体重
     Color favcolor;//喜欢的颜色
-    char name;
+    char *name; //经编译发现这儿不能直接用name,只能用 *name,不知道为什么？是否可以理解为char为一个类？人拥有名称这个属性
+    Dog *dog; //一个人一条狗，相当于狗也是人的属性，所以dog也是一个指针；
 }
 - (void)eat;
 - (void)run;
 - (void)print;
+
+- (void)rundog;
+- (void)feeddog;
 @end
 
 @implementation Student
 - (void) eat
 {
     weight += 1; //体重+1
-    NSLog(@"吃完这次后的体重为%f", weight);
+    NSLog(@"人吃完这次后的体重为%f", weight);
 }
 - (void) run
 {
     weight -= 1; //体重-1
-    NSLog(@"跑完这次后的体重为%f", weight);
+    NSLog(@"人跑完这次后的体重为%f", weight);
 }
 - (void)print
 {
     NSLog(@"姓名=%s,体重=%f,颜色=%d,性别=%d,生日=%d-%d-%d", name, weight, favcolor, sex,  birthday.year, birthday.mouth, birthday.day);
 }
+
+- (void)rundog{
+//    让狗跑起来
+    NSLog(@"遛狗...");
+    [dog run];
+}
+- (void)feeddog{
+//    让狗吃起来
+    NSLog(@"喂狗...");
+    [dog eat];
+}
 @end
 
+
 int main(){
-    Student *s = [Student new];
+    Student *s = [Student new]; //创建学生这个对象；
+    Dog *n = [Dog new];
+    n->curclor = ColorBlack;//一条黑狗
+    n->weight = 20; //一条10斤的狗
+    
+    s->dog = n;//把一条新的狗赋值给学生类中狗的属性；
     s->weight = 50;
     s->sex = SexMan;//性别赋值；
     //    s->birthday = {2012,9,12}; //编译报错，在定义结构体变量的时候才能这样写；
@@ -78,9 +121,13 @@ int main(){
     s->name = "jack";
     s->favcolor = ColorBlack;//赋值颜色
     
-    [s eat];
-    [s eat];
-    [s run];
-    [s run];
+//    [s eat];
+//    [s eat];
+//    [s run];
+//    [s run];
+//    [s print];
+
+    [s rundog];
+    [s feeddog];
     return 0;
 }
