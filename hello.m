d4b0c387b79dd0580e84659b96032d3e573d29a4
captainTao <578628438@qq.com>
2017-12-18 23:20:22 +0800

    ..

--- hello.m
+++ hello.m
@@ -815,3 +815,84 @@ int main()
 @implementation Student
 
 @end
+
+
+super对象
+
+/*
+ super的作用
+ 1.直接调用父类中的某个方法
+ 2.super处在对象方法中，那么就会调用父类的对象方法
+   super处在类方法中，那么就会调用父类的类方法
+ 
+ 3.使用场合：子类重写父类的方法时想保留父类的一些行为
+ */
+
+#import <Foundation/Foundation.h>
+
+// 僵尸
+@interface Zoombie : NSObject
+- (void)walk;
+
++ (void)test;
+- (void)test;
+
+@end
+
+@implementation Zoombie
+- (void)walk
+{
+    NSLog(@"往前挪两步******");
+}
+
++ (void)test
+{
+    NSLog(@"Zoombie+test");
+}
+
+- (void)test
+{
+    NSLog(@"Zoombie-test");
+}
+@end
+
+// 跳跃僵尸
+@interface JumpZoombie : Zoombie
++ (void)haha;
+- (void)haha2;
+@end
+
+
+@implementation JumpZoombie
+
++ (void)haha
+{
+    [super test];
+}
+
+- (void)haha2
+{
+    [super test];
+}
+
+- (void)walk
+{
+    // 跳两下
+    NSLog(@"跳两下");
+    
+    // 走两下(直接调用父类的walk方法)
+    [super walk];
+    //NSLog(@"往前挪两步----");
+
+}
+@end
+
+int main()
+{
+    //[JumpZoombie haha];
+    JumpZoombie *jz = [JumpZoombie new];
+    
+    [jz haha2];
+    
+    return 0;
+}
