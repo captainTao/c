27b6a10990f21543b6c54eefd0c254a5dd4e4990
captainTao <578628438@qq.com>
2018-01-25 23:05:02 +0800

    .

---
+++ UI.m
@@ -0,0 +1,38 @@
+
+/*
+ 1> UIButton    -> UIControl -> UIView
+ 
+ 1.1 设置控件的状态
+ 
+ 启用、禁用
+ @property(nonatomic,getter=isEnabled) BOOL enabled;
+ 选中、不选中
+ @property(nonatomic,getter=isSelected) BOOL selected;
+ 高亮或者不高亮
+ @property(nonatomic,getter=isHighlighted) BOOL highlighted;
+ 
+ 1.2 设置控件内容的布局
+ 垂直居中方向
+ @property(nonatomic) UIControlContentVerticalAlignment contentVerticalAlignment;
+ 水平居中方向
+ @property(nonatomic) UIControlContentHorizontalAlignment contentHorizontalAlignment;
+ 
+ 1.3 添加/删除监听方法
+ - (void)addTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents;
+ - (void)removeTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents;
+
+ 2> UILabel     -> UIView
+ 3> UIImageView -> UIView
+ 4> UITextField -> UIControl
+ 
+ *** 代理设计模式，在OC中，使用最为广泛的一种设计模式
+ 
+ 1> 代理的用处是什么？
+ *  监听那些不能通过addTarget监听的事件！
+ *  主要用来负责在两个对象之间，发生某些事件时，来传递消息或者数据
+ 
+ 2> 代理的实现步骤
+ (1)    成为(子)控件的代理，父亲（控制器）成为儿子（文本框）的代理
+ (2)    遵守协议->利用智能提示，快速编写代码
+ (3)    实现协议方法
+ */
