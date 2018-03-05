a92fd9b6da6759fd02eb532b36fbc1393b650d5c
captainTao <578628438@qq.com>
2018-03-05 23:35:28 +0800

    .

--- UI.m
+++ UI.m
@@ -61,6 +61,7 @@ cmd +1, cmd +2, cmd +3, 4, 5; 调整模拟器大小；
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    
      // 添加main.storyboard的背景颜色
      self.view.backgroundColor = [UIColor yellowColor];
      
@@ -74,6 +75,77 @@ cmd +1, cmd +2, cmd +3, 4, 5; 调整模拟器大小；
      CGFloat y = [UIScreen mainScreen].bounds.origin.y;
      NSLog(@"x = %f, y = %f", x, y);
      
+     // 添加一个色块：
+     UIView *viewcolor = [[UIView alloc]initWithFrame:CGRectMake(70, 100, 200, 50)];
+     //    UIView *viewcolor = [[UIView alloc]initWithFrame:CGRectMake(70, 100, 375-100-10, 50)];//375-100-10标识总的尺寸，然后左边留100，右边留10
+     viewcolor.backgroundColor = [UIColor purpleColor];
+     [self.view addSubview:viewcolor];
+     
+     CGFloat x = viewcolor.frame.origin.x;
+     CGFloat y = viewcolor.frame.origin.y;
+     NSLog(@"x=%f,y=%f",x,y);//x=70.000000,y=100.000000
+     NSLog(@"size=%@",NSStringFromCGSize(viewcolor.frame.size));//size={200, 50}
+     NSLog(@"bounds=%@",NSStringFromCGRect(viewcolor.bounds));//bounds={{0, 0}, {200, 50}}，相对于自己的宽高
+     NSLog(@"bounds=%@",NSStringFromCGRect(self.view.bounds));//bounds={{0, 0}, {375, 667}}，这是屏幕的尺寸
+     
+     
+     // view的增删改查：
+     UIView *viewcolor1 = [[UIView alloc]initWithFrame:CGRectMake(70, 100, 200, 50)];
+     viewcolor1.backgroundColor = [UIColor purpleColor];
+     [self.view addSubview:viewcolor1];
+     
+     UIView *viewcolor2 = [[UIView alloc]initWithFrame:CGRectMake(75, 105, 200, 50)];
+     viewcolor2.backgroundColor = [UIColor greenColor];
+     [self.view addSubview:viewcolor2];//在最后一个view后面添加
+     
+     //插入某个view到哪一层
+     [self.view insertSubview:viewcolor2 atIndex:2];
+     //view的交换,self.view的index为1，后面的子视图依次+1;
+     [self.view exchangeSubviewAtIndex:2 withSubviewAtIndex:3];
+     
+     UIView *viewcolor3 = [[UIView alloc]initWithFrame:CGRectMake(80, 110, 200, 50)];
+     viewcolor3.backgroundColor = [UIColor yellowColor];
+     [self.view addSubview:viewcolor3];//在最后一个view后面添加
+     
+     //调整到最后一个view
+     [self.view sendSubviewToBack:viewcolor3];
+     //调整到最前面来
+     [self.view bringSubviewToFront:viewcolor3];
+     //从视图中移除
+     [viewcolor3 removeFromSuperview];
+
+
+     //遍历：
+     viewcolor1.tag = 1;//添加tag
+     viewcolor2.tag = 2;
+     viewcolor3.tag = 3;
+     NSArray *subviewArray = self.view.subviews;//通过父视图获取子视图
+     for (UIView *view in subviewArray){ // 通过数组遍历
+         if(view.tag == 1){
+             view.backgroundColor = [UIColor grayColor];
+         }
+         if(view.tag == 2){
+             view.frame = CGRectMake(75, 105, 250, 70);
+         }
+     }
+
+
+    //修改view的透明度alpha,取值范围为0-1，数值越小，透明度越高；
+    _blue.alpha = 0.7;
+    //隐藏：hidden属性为bool类型，也可以设置它的透明度为0来把它隐藏
+    self.blue.hidden = false;
+    
+    //描边：
+    self.blue.layer.borderWidth = 3;
+    self.blue.layer.borderColor = [UIColor redColor].CGColor;//注意要转换为CGColor
+    //圆角：
+    self.blue.layer.cornerRadius = 10;
+    //阴影：
+    self.blue.layer.shadowColor = [UIColor grayColor].CGColor;
+    self.blue.layer.shadowOffset = CGSizeMake(10, 10); //设置shandow偏移量；
+    _blue.layer.shadowOpacity = 0.5; //设置透明度
+     
+     
      // 图片轮播效果
      UIImage *img1 = [UIImage imageNamed:@"img1.jpg"];
      UIImage *img2 = [UIImage imageNamed:@"img2.jpg"];
@@ -85,13 +157,10 @@ cmd +1, cmd +2, cmd +3, 4, 5; 调整模拟器大小；
      [self.view addSubview:imageview];
      //    [imageview release];
      
-     // 添加一个色块：
-     UIView *viewcolor = [[UIView alloc]initWithFrame:CGRectMake(70, 100, 200, 50)];
-     viewcolor.backgroundColor = [UIColor purpleColor];
-     // 把这个view添加到视图：
-     [self.view addSubview:viewcolor];
-     
+    
 }
+
+//////////////////////////////////////////////////////////////////////////
 
 /************************************************************/
 
@@ -161,26 +230,20 @@ startAnimating：方法，开始播放动画；
 stopAnimating：方法，停止播放动画；
 
 
-7.轮播效果：
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    UIImage *img1 = [UIImage imageNamed:@"img1.jpg"];
-    UIImage *img2 = [UIImage imageNamed:@"img2.jpg"];
-    UIImage *img3 = [UIImage imageNamed:@"img3.jpg"];
-    UIImageView *imageview = [[UIImageView alloc]initWithFrame:[[UIScreen mainScreen] bounds]];
-    imageview.animationImages = [NSArray arrayWithObjects:img1,img2,img3, nil];
-    imageview.animationDuration = 5;
-    [imageview startAnimating];
-    [self.view addSubview:imageview];
-//    [imageview release];
-}
+7.通过拖拽main.storyboard到viewcontoller中，在interface和end之间；
+形成如下：
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *blue;
+@end
 
-8.添加一个色块：
+拖拽的view一般我们写weak属性，否则比较容易形成强持有关系，很难被释放
+通过代码添加的空间，一般使用strong.
 
-     // 添加一个色块：
-     UIView *viewcolor = [[UIView alloc]initWithFrame:CGRectMake(70, 100, 200, 50)];
-     viewcolor.backgroundColor = [UIColor purpleColor];
-     // 把这个view添加到视图：
-     [self.view addSubview:viewcolor];
+通过代码链接之后，同样可以在viewcontroller中通过代码来修改控件
+self.blue.backgroundColor = [UIColor grayColor];
+
+
+
+
 
      
