dea0a52e582eb87b4f1b7cc158a7bd7ddf62224e
captainTao <578628438@qq.com>
2018-05-30 22:11:18 +0800

    .

--- Draw_Picture/Draw_Picture/Quartz.m
+++ Draw_Picture/Draw_Picture/Quartz.m
@@ -14,7 +14,7 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
-    [self drawPicture];
+    [self drawImageClip];
 }
 
 // 图片绘制
@@ -23,5 +23,16 @@
 //    [image drawAtPoint:CGPointMake(100, 100)];// 绘制的起始点坐标
 //    [image drawInRect:CGRectMake(30, 100, 300, 200)]; // 矩形中拉伸填充
     [image drawAsPatternInRect:CGRectMake(30, 100, 300, 500)]; // 图层的平铺
+}
+
+ // 图片的裁剪成圆形
+- (void)drawImageClip{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextAddEllipseInRect(context, CGRectMake(150, 150, 60, 60));
+    CGContextClip(context);
+    CGContextFillPath(context);
+    UIImage *image = [UIImage imageNamed:@"images/1.jpg"];
+    [image drawAtPoint:CGPointMake(150, 150)];
+    CGContextFillPath(context);
 }
 @end
