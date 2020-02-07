2c120846b6df173b6a484fb29416f8f2bc813122
captainTao <578628438@qq.com>
2020-02-07 17:52:42 +0800

    添加hello.swift的遗漏项

--- hello.swift
+++ hello.swift
@@ -120,7 +120,7 @@ print(i)
 
 
 // switch:
-// 1.不存在隐式的贯穿：不用加break,这个是q跟其他语言的区别
+// 1.不存在隐式的贯穿：不用加break,这个是跟其他语言的区别
 // 2.区间匹配
 let someInt = 3
 switch someInt {
@@ -151,3 +151,161 @@ case let(x, y) where x == -y:
 default:
     print("other")
 }
+
+// 控制权转移：
+continue
+break
+fallthrough
+return
+throw
+// continue, break用在for语句中
+let numbers = [1,2,3,4,5,6,7,8]
+for num in numbers{
+    if num == 5{
+        continue// break
+    }
+    print(num, terminator: "")
+}
+// fallthrough用在switch中
+// return用在函数中
+// throw用在try中
+
+
+
+
+// 函数
+// 无参，无返回
+func hello() {
+    print("hello,HH")
+}
+hello()
+// 有参，无返回
+func helloSB(person: String) {
+    print("hello,\(person)")
+}
+helloSB(person: "Captain")
+
+// 有参，有返回
+func Greeting(person: String, isGreeted: Bool) -> String{
+    return !isGreeted ? "hello,\(person)!" : "\(person) has gretted!"
+}
+print(Greeting(person: "Captain", isGreeted: true))
+print(Greeting(person: "Lemon", isGreeted: false))
+
+// 多参，多返回
+func mult(a: Int, b: Int) -> (product: Int, sum: Int){
+    return (a * b, a + b)
+}
+print(mult(a: 3, b: 4))
+
+// 函数的参数标签
+func Greet(name person: String, from place: String) -> String{
+    return "hello,\(person) from \(place)!"
+}
+print(Greet(name: "SongLi", from: "YiBing"))
+
+// 用 _可以省略输入函数标签name，from, 开发过程中经常省略第一个标签
+// 可以添加默认值
+func Greeta(_ person: String, _ place: String = "China") -> String{
+    return "hello, \(person) from \(place)!"
+}
+print(Greeta("Avatar", "360"))
+print(Greeta("QiQi"))
+
+
+
+
+
+
+// 函数作为参数
+func Sum(a:Double, b:Double) -> Double{
+    return a + b
+}
+
+func Product(a:Double, b:Double) ->Double{
+    return a * b
+}
+
+func MathFunc(_ Fun: (Double, Double) -> Double, figureOne a: Double, figureTwo b: Double) -> String{
+    return "Result = \(Fun(a, b))"
+}
+
+print(MathFunc(Sum(a:b:), figureOne: 3.4, figureTwo: 5.2))
+print(MathFunc(Product(a:b:), figureOne: 3.2, figureTwo: 2.5))
+
+
+// 函数作为返回类型
+let value = 3
+func forward(_ value: Int) -> Int {
+    return value + 1
+}
+
+func backforward(_ value: Int) -> Int {
+    return value - 1
+}
+
+func chooseFunc(_ value: Int) -> (Int) -> Int {
+    return value > 0 ? forward : backforward
+}
+
+let Function = chooseFunc(4)
+print(Function(4))
+print(chooseFunc(3)(-2)) //选择函数然后计算
+
+// 非固定参数
+func sumof(numbers: Int...) -> Int {
+    var sum = 0
+    for number in numbers{
+        sum += number
+    }
+    return sum
+}
+print(sumof(numbers: 42, 597, 12))
+print(sumof())
+
+
+
+// 类
+class Shape {
+    var numberOfSlides = 0
+    // 初始化方法，初始化的时候，需要传入初始化参数
+    // 初始化方法，也是构造方法
+    init(_ numberOfSliders: Int) {
+        self.numberOfSlides = numberOfSliders
+    }
+    func Description() -> String {
+        return "A shape of \(self.numberOfSlides)!"
+    }
+}
+
+//let shape = Shape(3)
+//shape.numberOfSlides = 5
+//print(shape.Description())
+
+// 可选链式调用
+var shape: Shape? = Shape(3)
+shape = nil
+// 为可选类型的时候，对象后面要加?
+print(shape?.Description())
+
+//继承
+class nameShape: Shape {
+    let name: String
+    var dot: Int
+    init(_ name: String, numberOfSliders: Int, dot: Int) {
+        self.name = name
+        self.dot = dot
+        super.init(numberOfSliders)
+    }
+    // 重写需要保持： 返回值类型
+    override func Description() -> String {
+        return "A \(self.dot) dot and \(self.numberOfSlides) sliders with the name: \(self.name) shape!"
+    }
+}
+
+var ccshape = nameShape("kk", numberOfSliders: 7, dot: 6)
+//var ccshape = nameShape(name: "cc", numberOfSliders: 7, dot: 3)
+//ccshape.name = "salanghei" // 常量不能更改
+ccshape.dot = 4
+print(ccshape.dot)
+print(ccshape.Description())
